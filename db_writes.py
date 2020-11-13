4ec73dd592b57b933dae78f12e53a23ded036266
Jason Morgado <ec2-user@ip-172-31-56-189.ec2.internal>
2020-11-13 22:39:08 +0000

    Linted db_writes.py

--- db_writes.py
+++ db_writes.py
@@ -1,21 +1,8 @@
 """
 Handles the database writes for app
 """
-import os
-import dotenv
-import psycopg2
-import flask_sqlalchemy
 import models
-from app import db 
-import urllib.parse as urlparse
-
-DATABASE_URI = os.environ["DATABASE_URL"]
-url = urlparse.urlparse(DATABASE_URI)
-
-SQL_USER = url.username
-SQL_PWD = url.password
-SQL_DB = url.path[1:]
-DBUSER = url.hostname
+from app import db
 
 def price_write(price_data):
     """Posts price history to DB"""
@@ -28,17 +15,18 @@ def price_write(price_data):
     poster = price_data['user']
     pfp = "temp profile picture"
     time = price_data['time']
-    db.session.add(models.Posts(item,imageurl,price_list_str, poster, pfp, time))
-    
-        
+    db.session.add(models.Posts(item, imageurl, price_list_str, poster, pfp, time))
+
+
 def get_posts(username):
     """fetches posts from DB"""
     rows = db.session.query.filter_by(username=username).all()
     item_data = {
-            'itemname': rows[0][1], 
-            'imgurl': rows[0][2], 
-            'pricehistory': rows[0][3], 
-            'user': rows[0][4], 
-            'pfp': rows[0][5],
-            'time': rows[0][6]
+        'itemname': rows[0][1],
+        'imgurl': rows[0][2],
+        'pricehistory': rows[0][3],
+        'user': rows[0][4],
+        'pfp': rows[0][5],
+        'time': rows[0][6]
     }
+    return item_data
